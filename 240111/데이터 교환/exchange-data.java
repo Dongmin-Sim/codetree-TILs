bbfcae3e864ae962cd71924857124e42f339d94e
Sim Dong Min <codongmin@gmail.com>
2024-01-11 20:12:48 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240111/데이터 교환/exchange-data.java
+++ 240111/데이터 교환/exchange-data.java
@@ -2,15 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 5, b = 6, c = 7;
-        int temp;
+        int temp = a;
 
-        temp = a;
-        a = b;
+        a = c;
+        c = b;
         b = temp;
 
-        temp = a;
-        a = c;
-        c = temp;
         
         System.out.println(a);
         System.out.println(b);
