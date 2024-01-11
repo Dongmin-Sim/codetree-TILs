e4f7d4ebd148d47907ba5c9c9f2dfb54908f069d
Sim Dong Min <codongmin@gmail.com>
2024-01-11 19:50:26 +0900

    Title: 달에서 무게 구하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240111/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int weight = 13;
+        double moonGravity = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", weight, moonGravity, (weight * moonGravity));
+
+    }
+}
