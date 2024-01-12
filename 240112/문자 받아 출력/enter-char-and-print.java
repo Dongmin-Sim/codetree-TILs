85cf88846b64482e82551e234146187d1c6b29bc
Sim Dong Min <codongmin@gmail.com>
2024-01-12 14:50:05 +0900

    Title: 문자 받아 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240112/문자 받아 출력/enter-char-and-print.java
+++ 240112/문자 받아 출력/enter-char-and-print.java
@@ -5,6 +5,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
-        
+        System.out.println(c);
     }
 }
