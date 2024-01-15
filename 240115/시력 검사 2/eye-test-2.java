43c426a447ff42a6074a9649c442cae74dfd76ff
Sim Dong Min <codongmin@gmail.com>
2024-01-15 23:56:58 +0900

    Title: 시력 검사 2, Time: 140ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240115/시력 검사 2/eye-test-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        double eye = sc.nextDouble();
+
+
+        if(eye >= 1.0) {
+            System.out.println("High");
+        } else if (eye >= 0.5) {
+            System.out.println("Middle");
+        } else {
+            System.out.println("Low");
+        }
+
+    }
+}
