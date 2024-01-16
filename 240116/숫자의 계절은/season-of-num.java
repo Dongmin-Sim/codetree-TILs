cea59146ded5218452f483a8920e66856cc80d65
Sim Dong Min <codongmin@gmail.com>
2024-01-16 13:08:53 +0900

    Title: 숫자의 계절은, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240116/숫자의 계절은/season-of-num.java
+++ 240116/숫자의 계절은/season-of-num.java
@@ -6,14 +6,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
 
-        if (m >= 3 && m < 6) {
-            System.out.println("Spring");
-        } else if (m >= 6 && m < 9) {
-            System.out.println("Summer");
-        } else if (m >= 9 && m <12) {
-            System.out.println("Fall");
-        } else {
+        if (m >= 12 || m <= 2) {
             System.out.println("Winter");
+        } else if (m <= 5) {
+            System.out.println("Spring");
+        } else if (m <= 8) {
+            System.out.println("Summer");
+        } else {
+            System.out.println("Fall");
         }
 
      
