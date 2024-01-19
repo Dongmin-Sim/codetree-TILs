b470738d7c7381df85b4d236ddac65e4d7611ec0
Sim Dong Min <codongmin@gmail.com>
2024-01-19 18:42:49 +0900

    Title: 369 게임, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240119/369 게임/369-game.java
+++ 240119/369 게임/369-game.java
@@ -6,13 +6,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-
         for(int i = 1; i <= n; i++) {
-            if(i%3==0 || (i%10!=0 && (i%10)%3 ==0)) {
+            if(i%3==0) {
+                System.out.print(0 + " ");
+            } else if((i%10!=0 && (i%10)%3 ==0)) {
+                System.out.print(0 + " ");
+            } else if ((i/10)%3==0 && (i/10)!=0) {
                 System.out.print(0 + " ");
             } else {
                 System.out.print(i + " ");
             }
+            
         }
     }
     // 1, 2, (3), 4, 5, (6), 7, 8, (9), 10, 11, (12), (13), 14, (15), (16), 17, (18), (19), 20 
