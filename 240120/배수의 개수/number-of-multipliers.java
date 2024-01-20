301ee9f30c2c25642552a3f78b23764957f9c964
Sim Dong Min <codongmin@gmail.com>
2024-01-20 14:36:32 +0900

    Title: 배수의 개수, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240120/배수의 개수/number-of-multipliers.java
+++ 240120/배수의 개수/number-of-multipliers.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n, cnt3, cnt5;
+        int n, cnt3 = 0, cnt5 = 0;
 
         for(int i = 0; i < 10; i++) {
             n = sc.nextInt();
             if(n % 3 == 0) {
                 cnt3++;
-            } else if (n % 5 == 0) {
+            }
+            if (n % 5 == 0) {
                 cnt5++;
             }
         }
