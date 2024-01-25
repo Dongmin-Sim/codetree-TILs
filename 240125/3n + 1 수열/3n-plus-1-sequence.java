5266fce38ddc7616cda05f8e6b200bf8c161b111
Sim Dong Min <codongmin@gmail.com>
2024-01-25 18:05:05 +0900

    Title: 3n + 1 수열, Time: 200ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240125/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        int n = sc.nextInt();
+
+        while(true) {    
+            if (n == 1) break;
+
+            if(n%2==0) {
+                n/=2;
+            } else {
+                n = n*3+1;
+            }
+            cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
