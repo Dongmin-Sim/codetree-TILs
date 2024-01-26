c37678be04011bd4b683de47e090051759068204
Sim Dong Min <codongmin@gmail.com>
2024-01-26 16:54:27 +0900

    Title: 모두 3의 배수, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240126/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        
+        for(int i = 0; i<5;i++) {
+            int n = sc.nextInt();
+
+            if(n%3!=0) {
+                flag = false;
+            }
+        }
+
+        if(flag) {
+            System.out.print(1);
+        }else {
+            System.out.print(0);
+        }
+    }
+}
