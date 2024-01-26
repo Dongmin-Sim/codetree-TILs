a03e77ac206397d27201389caeec7f86f97d62bc
Sim Dong Min <codongmin@gmail.com>
2024-01-26 16:50:30 +0900

    Title: 소수 판별기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/소수 판별기/prime-number-discriminator.java
+++ 240126/소수 판별기/prime-number-discriminator.java
@@ -6,10 +6,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        boolean flag = false;
+        boolean flag = true;
         for(int i = 2; i < n; i++){
             if(n%i==0) {
-                flag = true;
+                flag = false;
             }
         }
 
