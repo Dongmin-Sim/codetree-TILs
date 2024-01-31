0789d0782a5ee36c3dc3a3bb7bd59b4960d26fb2
Sim Dong Min <codongmin@gmail.com>
2024-01-31 16:52:17 +0900

    Title: 최대공약수 구하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240131/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -13,7 +13,7 @@ public class Main {
     public static void printGcd(int n, int m) {
         int gcd = 0;
 
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             if(n%i == 0 && m%i == 0){
                 if(i > gcd) {
                     gcd = i;
