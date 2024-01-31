fc81191d12a46c331158e7a4cb7ebe244948539e
Sim Dong Min <codongmin@gmail.com>
2024-01-31 18:12:48 +0900

    Title: 함수를 이용한 소수 판별, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240131/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean getPrime(int n) {
+        if(n == 1) return false;
         for(int i = 2; i < n; i++) {
             if(n%i==0) {
                 return false;
