b825e3d634cce2995b90685da8914da49c79a899
Sim Dong Min <codongmin@gmail.com>
2024-02-01 22:46:57 +0900

    Title: 두 수의 거듭제곱, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240201/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        System.out.print(power(a, b));
+
+    }
+
+    public static int power(int a, int b) {
+        int result = 1;
+        for(int i = 0; i < b; i++) {
+            result*=a;
+        }
+        return result;
+    }
+}
