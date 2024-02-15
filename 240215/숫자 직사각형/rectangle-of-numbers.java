9e83e6633d489df7b0eec82e0c306123d1ca0aca
Sim Dong Min <codongmin@gmail.com>
2024-02-15 23:30:43 +0900

    Title: 숫자 직사각형, Time: 254ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240215/숫자 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        int[][] matrix = new int[n][m];
+
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = num++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
