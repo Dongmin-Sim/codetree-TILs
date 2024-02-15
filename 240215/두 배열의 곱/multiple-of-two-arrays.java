c37062d83bebd95031358e7bfd643845324c7819
Sim Dong Min <codongmin@gmail.com>
2024-02-15 23:33:09 +0900

    Title: 두 배열의 곱, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = 3;
+        int[][] matrixA = new int[n][n];
+        int[][] matrixB = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrixA[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrixB[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrixA[i][j] * matrixB[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
