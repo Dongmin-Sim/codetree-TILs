22fd5f56414cae7929011a3a8253ab36ebcb5577
Sim Dong Min <codongmin@gmail.com>
2024-02-16 17:52:31 +0900

    Title: DateTime to DateTime, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240216/DateTime to DateTime/datetime-to-datetime.java
+++ 240216/DateTime to DateTime/datetime-to-datetime.java
@@ -7,22 +7,21 @@ public class Main {
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
         // 11 11:11 ~ a b:c
 
-        // System.out.println(changeToMinute(11, 11, 11));
+        int temp = changeToMinute(a, b, c) - changeToMinute(11, 11, 11);
+        if (temp > 0) {
+            System.out.println(temp);
+        } else {
+            System.out.println(-1);
+        }
 
-        // int temp = changeToMinute(a, b, c);
-        // if (temp > 0) {
-        //     System.out.println(temp);
-        // } else {
-        //     System.out.println(-1);
-        // } 
-        System.out.println(((a - 11 + 1) * 12 * 60) + ((b-11)*60) + c-11);
     }
 
     public static int changeToMinute(int day, int hour, int min) {
         int result = 0;
 
-        result += (hour * 60) + min;
-        result += (day * 12 * 60);
+        result += min;
+        result += hour * 60;
+        result += day * 24 * 60;
         return result;
     }
 }
