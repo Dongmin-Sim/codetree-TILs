7a48f1c5962ac31fc87ed2fd27b2dfa94e68d3d6
Sim Dong Min <codongmin@gmail.com>
2024-02-16 17:43:50 +0900

    Title: DateTime to DateTime, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240216/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        // 11 11:11 ~ a b:c
+
+        int temp = changeToMinute(a, b, c) + 720 - changeToMinute(11, 11, 11);
+        if (temp > 0) {
+            System.out.println(temp);
+        } else {
+            System.out.println(-1);
+        } 
+
+    }
+
+    public static int changeToMinute(int day, int hour, int min) {
+        int result = 0;
+
+        result += min;
+        result += hour * 60;
+        result += day * 12 * 60;
+        return result;
+    }
+}
