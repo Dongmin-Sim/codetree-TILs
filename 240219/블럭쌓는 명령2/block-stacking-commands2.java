3035a180b89cafa0fc203aad8edb333c579314a3
Sim Dong Min <codongmin@gmail.com>
2024-02-19 15:28:24 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240219/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] blockArr;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+
+        blockArr = new int[n];
+
+        for (int i = 0; i < k; i++) {
+            upBlock(sc.nextInt(), sc.nextInt());
+        }
+
+        int max = -1;
+        for (int i = 0; i < blockArr.length; i++) {
+            max = Math.max(max, blockArr[i]);
+        }
+        System.out.print(max);
+
+    }
+
+    public static void upBlock(int start, int end) {
+
+        for (int i = start; i <= end; i++) {
+            blockArr[i]++;
+        }
+    }
+}
