f440c36b55e304380f9adc9655195b3b86d7bbf8
Sim Dong Min <codongmin@gmail.com>
2024-02-19 15:42:05 +0900

    Title: 최대로 겹치는 지점, Time: 174ms, Memory: 10MB, Status: Passed - CodeTree

--- 240219/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 240219/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int[] arr = new int[100];
+    public static int[] arr = new int[100+1];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
