4df41e222f7e5267124c0e16b5546fcb9915472e
Sim Dong Min <codongmin@gmail.com>
2024-02-19 14:40:32 +0900

    Title: 여러가지 진수변환, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), b = sc.nextInt();
+
+        int[] arr = new int[1000];
+        int cnt = 0;
+
+        while(true) {
+            if (n < b) {
+                arr[cnt++] = n;
+                break;
+            }
+
+            arr[cnt++] = n % b;
+            n /= b;
+        }
+
+        for(int i = cnt - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+        
+    }
+}
