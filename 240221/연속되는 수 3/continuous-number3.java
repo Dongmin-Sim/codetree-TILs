498e5b12d0a4fec0df493c087e640222b4a01c7d
Sim Dong Min <codongmin@gmail.com>
2024-02-21 16:06:11 +0900

    Title: 연속되는 수 3, Time: 161ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240221/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int plus = 1;
+        int minus = 1;
+
+
+
+
+        // 양수 -> 양수
+        // 음수 -> 음수
+        int plusTemp = 1;
+        int minusTemp = 1;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) { // 1. 첫번째일때,
+                continue;
+            }
+
+            if ((array[i] * -1) > 0 && (array[i - 1] * -1) < 0) { // 3. 음수 -> 양수로 변경 시점
+                minusTemp = 1;    
+            } else { // 2. 양수 -> 음수로 변경 시점
+                plusTemp = 1;
+            }
+
+            if ((array[i] * -1) > 0 && (array[i - 1] * -1) > 0) {
+                plusTemp++;
+            } else {
+                minusTemp++;
+            }
+            
+            plus = Math.max(plusTemp, plus);
+            minus = Math.max(minusTemp, minus);
+
+        }
+
+        System.out.println(Math.max(plus, minus));
+    }
+}
