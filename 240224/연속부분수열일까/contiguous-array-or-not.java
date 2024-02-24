4671b69a29be872d060dd8bade962ec49cde9b53
Sim Dong Min <codongmin@gmail.com>
2024-02-24 17:25:14 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240224/연속부분수열일까/contiguous-array-or-not.java
+++ 240224/연속부분수열일까/contiguous-array-or-not.java
@@ -16,25 +16,34 @@ public class Main {
             arrB[i] = sc.nextInt();
         }
 
-        int idxB = 0;
+        int idxA = 0, idxB = 0;
 
-        for (int i = 0; i < n1; i++) {
-            if (idxB == n2 - 1) {
+        boolean flag = false;
+        while (true) {
+            // 남은 검사 수가 B보다도 작고, 맞지 않을때 , 더 이상 진행할 필요 없음.
+            if (n1 - idxA < n2 && arrA[idxA] != arrB[idxB]) {
                 break;
             }
-            if (arrA[i] == arrB[idxB]) {
+            // B가 부분수열일 경우 종료
+
+            if (arrA[idxA] == arrB[idxB]) {
+                if (idxB == n2 - 1) {
+                    flag = true;
+                    break;
+                }
+                idxA++;
                 idxB++;
             } else {
+                idxA++;
                 idxB = 0;
             }
 
         }
 
-        if (idxB != n2 - 1) {
-            System.out.println("No");
-        } else {
+        if (flag) {
             System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
-
     }
 }
