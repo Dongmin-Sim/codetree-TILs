fbf5b862da199aeddbece313a850a98e6adcc68e
Sim Dong Min <codongmin@gmail.com>
2024-03-04 21:35:32 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 244ms, Memory: 13MB, Status: Passed - CodeTree

--- 240304/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 240304/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int MAX = 1000;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
+
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int idx = n;
+        int cnt = 0;
+        int[] idxArr = new int[n];
 
-        while (true) {
-            int max = Integer.MIN_VALUE;
-            int tempIdx = 0;
-            for (int i = 0; i < idx; i++) {
-                if (max < arr[i]) {
-                    max = arr[i];
-                    tempIdx = i;
-                }
+        idxArr[cnt++] = 0;
+
+        for (int i = 1; i < n; i++) {
+            int lastIdx = idxArr[cnt - 1];
+            if (arr[i] > arr[lastIdx]) {
+                idxArr[cnt++] = i;
             }
-            if (tempIdx == 0) {
-                System.out.print(1);
-                break;
-            }
-            System.out.print(tempIdx + 1 + " ");
-            idx = tempIdx;
         }
 
+        for (int i = cnt - 1; i >= 0; i--) {
+            System.out.print((idxArr[i] + 1) + " ");
+        }
     }
 }
