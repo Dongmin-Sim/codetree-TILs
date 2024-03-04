e8c9dc8b9c1fc02792189840492ae77439d59c70
Sim Dong Min <codongmin@gmail.com>
2024-03-04 21:45:42 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 214ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240304/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n-1; i++) {
+            int maxVal = 0;
+            for (int j = i+1; j < n; j++) {
+                maxVal = Math.max(maxVal, prices[j] - prices[i]);
+            }
+
+            answer = Math.max(answer, maxVal);
+        }
+        System.out.println(answer);
+    }
+}
