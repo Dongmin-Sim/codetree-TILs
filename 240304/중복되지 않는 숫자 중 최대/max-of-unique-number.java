f57035d7707bef56e3231ff7688a2f3b813dec2a
Sim Dong Min <codongmin@gmail.com>
2024-03-04 21:04:50 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 201ms, Memory: 13MB, Status: Passed - CodeTree

--- 240304/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240304/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -2,33 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static int MAX = 1000;
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
 
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int[] arr = new int[n];
+        int[] cnt = new int[MAX+1];
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int max = -1;
-
         for (int i = 0; i < n; i++) {
-            int maxCandidate = arr[i];
+            cnt[arr[i]]++;
+        }
 
-            if (max < maxCandidate) {
-                int cnt = 0;
-                for (int j = 0; j < n; j++) {
-                    if (arr[j] == maxCandidate) {
-                        cnt++;
-                    }
-                }
-
-                if (cnt == 1) {
-                    max = maxCandidate;
-                }
+        int max = -1;
+        for (int maxCandidate = MAX; maxCandidate >= 0; maxCandidate--) {
+            if (cnt[maxCandidate] == 1) {
+                max = maxCandidate;
+                break;
             }
         }
         System.out.println(max);
