b0f33578f1cf211d2bd59fb3c3667137d0a9f313
Sim Dong Min <codongmin@gmail.com>
2024-03-04 22:03:27 +0900

    Title: 동전이 있는 위치, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240304/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < m; i++) {
+            int r = sc.nextInt() - 1, c = sc.nextInt() - 1;
+            matrix[r][c]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
