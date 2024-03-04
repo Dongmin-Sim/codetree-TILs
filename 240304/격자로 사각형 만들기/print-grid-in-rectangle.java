701efb6cef21e5e9b2e0570b620ef2aaaa6360f9
Sim Dong Min <codongmin@gmail.com>
2024-03-04 22:07:31 +0900

    Title: 격자로 사각형 만들기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240304/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            matrix[i][0] = 1;
+            matrix[0][i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1] + matrix[i - 1][j - 1];                 
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
