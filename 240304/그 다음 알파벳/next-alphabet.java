3d3add311d90a22efd7a11b9569132738633c3b5
Sim Dong Min <codongmin@gmail.com>
2024-03-04 11:37:46 +0900

    Title: 그 다음 알파벳, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240304/그 다음 알파벳/next-alphabet.java
+++ 240304/그 다음 알파벳/next-alphabet.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        char a = sc.next().charAt(0);
+        int a = sc.next().charAt(0) + 1;
 
-        System.out.println((char) (a + 1));
+        System.out.println((char) ((a / 123) * 'a' + (a % 123)));
+
+
     }
 }
