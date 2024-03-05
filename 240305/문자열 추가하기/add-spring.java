4307458dde2ba10b40a1528189ec86c3c4ffea35
Sim Dong Min <codongmin@gmail.com>
2024-03-05 15:28:23 +0900

    Title: 문자열 추가하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/문자열 추가하기/add-spring.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+
+        System.out.println(word+"Hello");
+    }
+}
