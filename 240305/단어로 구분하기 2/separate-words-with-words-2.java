7010bc4c1fd7c52827c0b880d9736113ecea8aca
Sim Dong Min <codongmin@gmail.com>
2024-03-05 14:44:47 +0900

    Title: 단어로 구분하기 2, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String[] sents = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            sents[i] = sc.next();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(sents[i]);
+            }
+        }
+    }
+}
