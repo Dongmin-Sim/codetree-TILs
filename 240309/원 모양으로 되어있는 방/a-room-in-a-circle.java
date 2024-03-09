709c5bedd96eae03f1d540d9912c8da4c98e9aa0
Sim Dong Min <codongmin@gmail.com>
2024-03-09 23:23:32 +0900

    Title: 원 모양으로 되어있는 방, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240309/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n;
+    public static int[] arr;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int minDist = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            minDist = Math.min(minDist, getDist(i));
+        }
+        System.out.println(minDist);
+    }
+    private static int getDist(int i) {
+        int total = 0;
+        int multiply = 1;
+        for (int j = i; j < i + n; j++) {
+            if (j == i) {
+                continue;
+            }
+            total += arr[j % 5] * (multiply++);
+        }
+
+        return total;
+    }
+}
