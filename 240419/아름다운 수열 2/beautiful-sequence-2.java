5c5b40338535d47d86694394846dc6f852b8089b
Sim Dong Min <codongmin@gmail.com>
2024-04-19 14:16:12 +0900

    Title: 아름다운 수열 2, Time: 276ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240419/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static int N;
+    public static int M;
+
+    public static int[] a;
+    public static int[] b;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        a = new int[N];
+        b = new int[M];
+        for (int i = 0; i < N; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < M; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N - M + 1; i++) {
+            int[] exist = new int[M];
+            // a 수열과 b 수열이 같은지
+            for (int j = i; j < i+M; j++) {
+                int num = a[j];
+
+                for (int k = 0; k < M; k++) {
+                    if (exist[k] != 1 && num == b[k]) {
+                        exist[k] = 1;
+                        break;
+                    }
+                }
+            }
+            if (Arrays.stream(exist).sum() == 3) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
