cdcbe7832b37c9cd3d12276d959feca98e0d37cd
Sim Dong Min <codongmin@gmail.com>
2024-04-26 12:08:48 +0900

    Title: 겹치지 않는 선분 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240426/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] x1Arr;
+    public static int[] x2Arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        x1Arr = new int[n];
+        x2Arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x1Arr[i] = sc.nextInt();
+            x2Arr[i] = sc.nextInt();
+        }
+
+        // 두 선분이 교차하는 조건
+        // (x1, 0), (x2, 1) ... (x3, 0), (x4, 1)
+        // x1 < x3 && x2 > x4
+        // x1 > x3 && x2 < x4
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int x1 = x1Arr[i], x2 = x2Arr[i];
+                int x3 = x1Arr[j], x4 = x2Arr[j];
+
+                if ((x1 < x3 && x2 > x4) || (x1 > x3 && x2 < x4)) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(n - count*2);
+    }
+}
