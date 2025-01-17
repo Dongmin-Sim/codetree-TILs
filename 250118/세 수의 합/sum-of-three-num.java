2f5350c707d17f1d03344a2107822cb5e8471ef8
Sim Dong Min <codongmin@gmail.com>
2025-01-18 00:19:08 +0900

    Title: 세 수의 합, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250118/세 수의 합/sum-of-three-num.java
+++ 250118/세 수의 합/sum-of-three-num.java
@@ -16,7 +16,7 @@ public class Main {
             map.put(array[i], map.getOrDefault(array[i], 0)+1);
         }
 
-        for(int i=0; i<n-1; i++) {
+        for(int i=0; i<n; i++) {
             map.put(array[i], map.get(array[i]) - 1);
 
             for(int j=0; j<i; j++) {
