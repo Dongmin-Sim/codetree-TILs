bf82767f93fe764f995557942ca47b074144726c
Sim Dong Min <codongmin@gmail.com>
2025-01-18 00:16:07 +0900

    Title: 세 수의 합, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250118/세 수의 합/sum-of-three-num.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = 0;
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] array = new int[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            array[i] = sc.nextInt();
+            map.put(array[i], map.getOrDefault(array[i], 0)+1);
+        }
+
+        for(int i=0; i<n-1; i++) {
+            map.put(array[i], map.get(array[i]) - 1);
+
+            for(int j=0; j<i; j++) {
+                int temp = k - (array[i] + array[j]);
+                if (map.containsKey(temp) && map.get(temp) > 0) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
