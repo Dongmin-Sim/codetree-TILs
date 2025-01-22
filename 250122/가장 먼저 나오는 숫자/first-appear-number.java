b4e9914a7972d2ae8154e8fa652fbba26bae43b3
Sim Dong Min <codongmin@gmail.com>
2025-01-22 09:38:32 +0900

    Title: 가장 먼저 나오는 숫자, Time: 121ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250122/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int[] array;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        array = new int[n];
+
+        for(int i=0; i<n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<m; i++) {
+            lowerUpper(sc.nextInt());
+        }
+    }
+
+    public static void lowerUpper(int num){
+        int l = 0, r = array.length;
+
+        while(l<r) {
+            int m = l + (r - l) / 2;
+            if (array[m] >= num) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        if (array[l] == num) {
+            System.out.println(l+1);
+            return;
+        }
+        System.out.println(-1);
+    }
+}
